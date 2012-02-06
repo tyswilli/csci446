7b7560733d0f32e5ac8dbeb7867da9b5cce7faff
Tyson Williams <tyswilli@mymail.mines.edu>
2012-02-06 10:14:46 -0700

    completed instructions up to 6.2 in the book.

--- project03/depot/db/migrate/20120206165515_create_products.rb
+++ project03/depot/db/migrate/20120206165515_create_products.rb
@@ -4,7 +4,7 @@ class CreateProducts < ActiveRecord::Migration
       t.string :title
       t.text :description
       t.string :img_url
-      t.decimal :price
+      t.decimal :price, precision: 8, scale: 2
 
       t.timestamps
     end
