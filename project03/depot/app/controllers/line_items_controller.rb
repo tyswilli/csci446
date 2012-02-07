1342f6433919f1fe10b9a50c427a9977e752fb6d
Tyson Williams <tyswilli@mymail.mines.edu>
2012-02-06 23:46:54 -0700

    chapter 14 completed... lets see if i can finish 16

--- project03/depot/app/controllers/line_items_controller.rb
+++ project03/depot/app/controllers/line_items_controller.rb
@@ -1,4 +1,6 @@
 class LineItemsController < ApplicationController
+    skip_before_filter :authorize, only: :create
+
   # GET /line_items
   # GET /line_items.json
   def index
@@ -46,7 +48,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
-		format.js   { @current_item = @line_item }
+        format.js   { @current_item = @line_item }
         format.json { render json: @line_item,
           status: :created, location: @line_item }
       else
--- project03/depot/app/controllers/orders_controller.rb
+++ project03/depot/app/controllers/orders_controller.rb
@@ -1,103 +1,88 @@
-#---
-# Excerpted from "Agile Web Development with Rails",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
-#---
-class OrdersController < ApplicationController
-  # GET /orders
-  # GET /orders.json
+class LineItemsController < ApplicationController
+    skip_before_filter :authorize, only: :create
+
+  # GET /line_items
+  # GET /line_items.json
   def index
-    @orders = Order.paginate page: params[:page], order: 'created_at desc', per_page: 10
+    @line_items = LineItem.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @orders }
+      format.json { render json: @line_items }
     end
   end
 
-  # GET /orders/1
-  # GET /orders/1.json
+  # GET /line_items/1
+  # GET /line_items/1.json
   def show
-    @order = Order.find(params[:id])
+    @line_item = LineItem.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @order }
+      format.json { render json: @line_item }
     end
   end
 
-  # GET /orders/new
-  # GET /orders/new.json
+  # GET /line_items/new
+  # GET /line_items/new.json
   def new
-    @cart = current_cart
-    if @cart.line_items.empty?
-      redirect_to store_url, notice: "Your cart is empty"
-      return
-    end
-
-    @order = Order.new
+    @line_item = LineItem.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @order }
+      format.json { render json: @line_item }
     end
   end
 
-  # GET /orders/1/edit
+  # GET /line_items/1/edit
   def edit
-    @order = Order.find(params[:id])
+    @line_item = LineItem.find(params[:id])
   end
 
-  # POST /orders
-  # POST /orders.json
+  # POST /line_items
+  # POST /line_items.json
   def create
-    @order = Order.new(params[:order])
-    @order.add_line_items_from_cart(current_cart)
-
+    @cart = current_cart
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product.id)
     respond_to do |format|
-      if @order.save
-        Cart.destroy(session[:cart_id])
-        session[:cart_id] = nil
-        format.html { redirect_to store_url, notice: 
-          'Thank you for your order.' }
-        format.json { render json: @order, status: :created,
-          location: @order }
+      if @line_item.save
+        format.html { redirect_to store_url }
+        format.js   { @current_item = @line_item }
+        format.json { render json: @line_item,
+          status: :created, location: @line_item }
       else
-        @cart = current_cart
         format.html { render action: "new" }
-        format.json { render json: @order.errors,
+        format.json { render json: @line_item.errors,
           status: :unprocessable_entity }
       end
     end
   end
 
-  # PUT /orders/1
-  # PUT /orders/1.json
+  # PUT /line_items/1
+  # PUT /line_items/1.json
   def update
-    @order = Order.find(params[:id])
+    @line_item = LineItem.find(params[:id])
 
     respond_to do |format|
-      if @order.update_attributes(params[:order])
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
+      if @line_item.update_attributes(params[:line_item])
+        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /orders/1
-  # DELETE /orders/1.json
+  # DELETE /line_items/1
+  # DELETE /line_items/1.json
   def destroy
-    @order = Order.find(params[:id])
-    @order.destroy
+    @line_item = LineItem.find(params[:id])
+    @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to orders_url }
+      format.html { redirect_to line_items_url }
       format.json { head :ok }
     end
   end
