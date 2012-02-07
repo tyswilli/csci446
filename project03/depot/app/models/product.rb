1425b325b7295e07fcade9c0cf3a4ba07bedf7d1
Tyson Williams <tyswilli@mymail.mines.edu>
2012-02-06 18:33:19 -0700

    Chapter 7 completed.

--- project03/depot/app/models/product.rb
+++ project03/depot/app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
 validates :title, :description, :img_url, presence: true
 validates :price, numericality: {greater_than_or_equal_to: 0.01}
+validates :title, uniqueness: true
 validates :img_url, allow_blank:true, format: {
 	with: %r{\.(gif|png|jpg)$}i,
 	message: 'must be a url for GIF, JPG, or PNG image'
