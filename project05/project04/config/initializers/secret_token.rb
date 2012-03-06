3ea6a7c147e0e61fca88f5cb281afc7c243637a3
Tyson Williams <tyswilli@mymail.mines.edu>
2012-03-05 23:38:34 -0700

    Deleted duplicate files

--- project05/project04/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Articles::Application.config.secret_token = '53b97b475442df6614b92c2786d052a7507993bfbfad7e3cfd4afc82b76a6f1ec2f87bf0207d0a0cbf8ebdb923fe1d42b3ef350526d2243a54c80c7523ae59b7'
