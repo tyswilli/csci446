324ed2d295602eb61233007f602051aead10e16d
Tyson Williams <tyswilli@mymail.mines.edu>
2012-03-28 20:14:13 -0600

    built the users scaffold and installed all the relevant gems

---
+++ project06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamez::Application.config.secret_token = '8c27af24b8c98401f2e7078a02dd489ade9b4da8e689054a204082e4671911a04bb7c124a0bea74a4d2a5c103d3243aadb0e5928c161f4f74c0d1598d62df7af'
