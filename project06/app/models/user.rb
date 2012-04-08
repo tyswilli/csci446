4afa3ba6341056eb312dd15d13c5701b53e8cfb5
Tyson Williams <tyswilli@mymail.mines.edu>
2012-04-07 22:44:33 -0600

    login screen and new user screen working.

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :username, :email, :password
-  acts_as_authentic
+  acts_as_authentic do |config|
+     config.merge_validates_length_of_password_field_options :minimum => 6
+  end
 end
--- project06/config/routes.rb
+++ project06/config/routes.rb
@@ -1,8 +1,10 @@
 Gamez::Application.routes.draw do
   get "users_sessions/new"
-
+  resources :user_sessions
   resources :users
-
+  match '/login', :controller => 'user_sessions', :action => 'new', :as => 'login'
+  match '/logout', :controller => 'user_sessions', :action => 'destroy', :as => 'logout'
+ 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
